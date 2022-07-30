a5a714993977293e0c2733dc6a666fcf365aee0d
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-30 17:13:07 +0900

    게시물 객체 3개 노출하기.

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -1,10 +1,19 @@
 package com.ll.exam.article;
 
 import com.ll.exam.Rq;
+import com.ll.exam.article.dto.ArticleDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArticleController {
     public void showList(Rq rq) {
-        rq.println("리스트페이지");
+        List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDtos.add(new ArticleDto(1, "제목1", "내용1"));
+        articleDtos.add(new ArticleDto(2, "제목2", "내용2"));
+        articleDtos.add(new ArticleDto(3, "제목3", "내용3"));
 
+        rq.setAttr("articles", articleDtos);
+        rq.view("usr/article/list");
     }
 }
